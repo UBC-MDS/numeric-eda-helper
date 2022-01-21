aa8ba07146e135f97ef151bed4b13cbe27813d8f
michelle-wms <meisi.wg@gmail.com>
2022-01-20 20:20:55 -0800

    Added function and tests

--- tests/test_flag_outliers.py
+++ tests/test_flag_outliers.py
@@ -10,13 +10,12 @@ def test_flag_outliers():
                     'col2': [1,2,3,4,5,6,7,8,9,10,11,12,13],
                     'col3': [-50, 1,2,3,4,5,6,7,8,9,10,11,50000]})
     output1 = flag_outliers(df, threshold=0.1)
-    output2 = flag_outliers(df, threshold=0.2)
 
     # Assert statements 
-    assert len(output1) == 2, "Output should contain 2 variables!"
-    assert len(output2) == 1, "Output should contain only 1 variable!"
+    assert len(output1) == 2, "Output should return 2 variables!"
+    assert flag_outliers(df, 1.0) == {}, "Output should return an empty dictionary"
     assert output1 == {'col1': 0.23076923076923078, 'col3': 0.15384615384615385}, "Output is incorrect"
-    assert type(output2) == dict, "The output should be a dictionary"
+    assert type(output1) == dict, "The returned output datatype should be a dictionary"
 
     # Raise specific type errors
     with pytest.raises(TypeError):
