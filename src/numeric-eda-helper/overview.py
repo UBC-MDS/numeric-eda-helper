12bc64908117cae9f85c79e54a68542e671ad986
Sam Quist <lummox9@gmail.com>
2022-01-21 15:33:43 -0800

    add body to overview function

--- src/numeric-eda-helper/overview.py
+++ src/numeric-eda-helper/overview.py
@@ -1,14 +1,14 @@
-def overview(input, verbose=False, quiet=False):
+import numpy as np
+import pandas as pd
+
+def overview(data, quiet=False):
     """Gives a statistical overview of the input data.
     Returns a `pandas.DataFrame` of descriptive statistical values.
     
     Parameters
     ----------
-    input : `pandas.DataFrame` or `np.array`
+    data : `pandas.DataFrame`
         Input data to be summarized.
-    verbose : `bool`, default 'False`
-        Boolean value corresponding to showing additional
-        information (histogram, estimated distribution).
     quiet : `bool`, default 'False'
         Boolean value corresponding to showing output of 
         the function. Used for acquiring variables.
@@ -20,5 +20,33 @@ def overview(input, verbose=False, quiet=False):
 
     Examples
     --------
-    >>> overview(dataframe, verbose=True, quiet=False)
+    >>> import pandas as pd
+    >>> import numpy as np
+    >>> overview(dataframe, quiet=False)
     """
+    if not isinstance(data, pd.DataFrame):
+        raise TypeError("Input data must be `pandas.DataFrame` object")
+
+    if not isinstance(quiet, bool):
+        raise TypeError('Parameter "quiet" must be of `boolean` type.')
+    means = []
+    stds = []
+    variances = []
+    medians = []
+    stat_vals = [means, stds, variances, medians]
+    stat_names = ['mean_', 'std_', 'var_', 
+                 'median_']
+    for col in range(len(data.columns)):
+        means.append(np.mean(data[data.columns[col]]))
+        medians.append(np.median(data[data.columns[col]]))
+        stds.append(np.std(data[data.columns[col]]))
+        variances.append(np.var(data[data.columns[col]]))
+
+    if quiet:
+        for i in range(4):
+            globals()[stat_names[i]] = stat_vals[i]
+            print("Global variable '{}' created.".format(stat_names[i]))
+
+    else:
+        return pd.DataFrame(data = stat_vals,
+                        columns=data.columns, index=stat_names)
